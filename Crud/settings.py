d35f3b4b4ff7653f8dcdd0a7c26fe9364348976c
Mahfujul-hoque <mdmahfujulhoque123@gmail.com>
2023-11-10 23:04:23 +0600

    done vercel setting

--- Crud/settings.py
+++ Crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a&ca%j1vht8+r_jbhloqajdn0%9ch615zv^bp2u#spi3(q-c!2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -116,10 +116,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
